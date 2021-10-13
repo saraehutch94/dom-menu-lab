f66c0899f88784ffcada739986da4b338bcea4c8
Sara Hutchinson <sarahutchinson@Saras-MacBook-Pro-2.local>
2021-10-13 13:10:35 -0400

    change innerHTML of main element via the DOM

--- js/script.js
+++ js/script.js
@@ -8,4 +8,9 @@ const mainEl = document.querySelector("main");
 // Task 1.1
 
 mainEl.style.backgroundColor = "var(--main-bg)";
-console.log(mainEl);
+// console.log(mainEl);
+
+// Task 1.2
+
+mainEl.innerHTML = "<h1>SEI Rocks!</h1>";
+// console.log(mainEl);
